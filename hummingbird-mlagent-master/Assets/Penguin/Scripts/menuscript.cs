1e972c671c99c405f336406528ee71ea904191fc
SyedMinhalAbbas <64733427+SyedMinhalAbbas@users.noreply.github.com>
2020-07-12 15:43:23 +0500

    Assignment 4b

---
+++ hummingbird-mlagent-master/Assets/Penguin/Scripts/menuscript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void changemenuscene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+
+    }
+}
